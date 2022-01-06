c5d1b7f3ca5df14f6ff974b290125dbbee110f7f
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-06 19:43:58 +0700

    create model Quote and example quotes

--- app/src/main/java/com/example/quotedroid/MainActivity.kt
+++ app/src/main/java/com/example/quotedroid/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.quotedroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quotedroid.models.Quote
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quotes = listOf<Quote>(
+            Quote(
+                text = "Genius is one percent inspiration and ninety-nine percent perspiration.Genius is one percent inspiration and ninety-nine percent perspiration.Genius is one percent inspiration and ninety-nine percent perspiration.",
+                author = "Thomas Edison"
+            ),
+            Quote(
+                text = "You can observe a lot just by watching.",
+                author = "Yogi Berra"
+            ),
+            Quote(
+                text = "A house divided against itself cannot stand.",
+                author = "Abraham Lincoln"
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/example/quotedroid/models/Quote.kt
@@ -0,0 +1,6 @@
+package com.example.quotedroid.models
+
+data class Quote (
+    val text: String,
+    val author: String,
+)
